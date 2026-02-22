fb3674b0b41039549d0c8ffd9142e35882309ee0
libantain <libantain@noreply.gitcode.com>
2026-02-22 15:57:14 +0800

    222

--- Hazel/vendor/assimp/revision.h
+++ Hazel/vendor/assimp/revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0xe0fcb6bc
+#define GitVersion 0x7ad6c921
 #define GitBranch "master"
 
 #define VER_MAJOR 5
@@ -16,7 +16,7 @@
 #if (GitVersion == 0)
 #define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD)
 #else
-#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit e0fcb6bc)"
+#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit 7ad6c921)"
 #endif
 #define VER_COPYRIGHT_STR           "\xA9 2006-2022"
 
